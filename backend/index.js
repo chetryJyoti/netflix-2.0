df9911d5e468ae00c851a1edaee8f66d1e5bdd79
Gaurabjyoti Buragohain <gaurabburagohain2001@gmail.com>
2024-09-16 23:06:43 +0530

    config changes

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://netflix-2-0-rhqe.onrender.com',
+    origin:'https://netflix-2-0-frontend.vercel.app/',
     credentials:true,
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     withCredentials: true,
@@ -31,6 +31,9 @@ app.use(cors(corsOptions));
 // api
 app.use("/api/v1/user", userRoute);
 app.use("/api/movies",movieRoute);
+// app.use("/home",(req,res)=>{
+//     res.send("Hello gaurab!!");
+// })
 
 app.listen(process.env.PORT || 8080,() => {
     console.log(`Server running at port 3001`);
